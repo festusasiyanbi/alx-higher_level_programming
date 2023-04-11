6df1345feeea5c5b0cbaa8813d8b5883ca5f8616
Festus <festusasiyanbi80@gmail.com>
2023-04-11 21:37:45 +0000

    I corrected some errors

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
+const line1 = "C is fun";
+const line2 = "Python is cool";
+const line3 = "JavaScript is amazing";
+console.log(line1 + "\n");
+console.log(line2 + "\n");
+console.log(line3 + "\n");
 
-const myVar = "C is fun\n Python is cool\nJavaScript is amazing";
-console.log(myVar)
