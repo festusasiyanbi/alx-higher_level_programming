f0bca2d890d9a375f69f7f553850bcb9f283a04b
Festus <festusasiyanbi80@gmail.com>
2023-04-11 22:24:17 +0000

    I corrected an error

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,16 +1,8 @@
 #!/usr/bin/node
 
 function add(a, b) {
-  return a + b;
-}
-
-const num1 = parseInt(process.argv[2]);
-const num2 = parseInt(process.argv[3]);
-
-if (isNaN(num1) || isNaN(num2)) {
-  console.log("Invalid input, both arguments must be integers.");
-} else {
-  const result = add(num1, num2);
+  const result = a + b;
   console.log(result);
+  return result;
 }
 
