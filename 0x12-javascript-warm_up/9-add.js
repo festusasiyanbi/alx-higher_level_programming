669dcc78355db52729a60215a234af8cac94fe7c
Festus <festusasiyanbi80@gmail.com>
2023-04-11 22:25:57 +0000

    I corrected an error

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,17 @@
 #!/usr/bin/node
 
 function add(a, b) {
-  const result = a + b;
-  console.log(result);
-  return result;
+  return a + b;
 }
+
+const firstArg = parseInt(process.argv[2]);
+const secondArg = parseInt(process.argv[3]);
+
+if (isNaN(firstArg) || isNaN(secondArg)) {
+  console.log('Please provide two integer arguments.');
+} else {
+  const result = add(firstArg, secondArg);
+  console.log(result);
+}
+
 
