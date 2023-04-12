9c4c70321d5a00967cef8afc81460862a67819b6
Festus <festusasiyanbi80@gmail.com>
2023-04-12 15:24:51 +0000

    I added the advanced tasks

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const sourceFile1 = process.argv[2];
+const sourceFile2 = process.argv[3];
+const destFile = process.argv[4];
+
+const data1 = fs.readFileSync(sourceFile1);
+const data2 = fs.readFileSync(sourceFile2);
+
+const concatenatedData = Buffer.concat([data1, data2]);
+
+fs.writeFileSync(destFile, concatenatedData);
+
+console.log(`Concatenated ${sourceFile1} and ${sourceFile2} to ${destFile}`);
+
